4ed224e72846c2abe7145176f1dd1e3e6c60bffd
Sean Ru <64388316+sean-ru@users.noreply.github.com>
2022-06-17 12:01:34 -0500

    compiles and works now

--- Priority_Queues_and_Heaps/join_ropes.cpp
+++ Priority_Queues_and_Heaps/join_ropes.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 class Solution {
 public:
     int join_ropes(vector<int>& rope_lengths) {
-        
+        priority_queue<int, vector<int>, std::greater<int> > pq;
+        int cost = 0;
+        while (pq.size() > 1) {
+            int rope1 = pq.top();
+            pq.pop();
+            int rope2 = pq.top();
+            pq.pop();
+            pq.push(rope1 + rope2);
+            cost += (rope1 + rope2);
+        }
+        return cost;
     }
 };
