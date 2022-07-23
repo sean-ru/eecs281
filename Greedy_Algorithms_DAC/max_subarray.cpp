e73f04fcc3922b411f742ea0f6a8db59b0fdc5de
Sean Ru <64388316+sean-ru@users.noreply.github.com>
2022-07-23 17:38:27 -0500

    compiles and works

---
+++ Greedy_Algorithms_DAC/max_subarray.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int localMax = 0;
+        int globalMax = INT_MIN;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            localMax = max(nums[i], nums[i] + localMax);
+            if (localMax > globalMax) {
+                globalMax = localMax;
+            }
+        }
+        return globalMax;
+    }
+};
