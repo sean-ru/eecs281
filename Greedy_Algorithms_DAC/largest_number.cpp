b8e2e65631e35d30b650d4a00a921746d30f5b8a
Sean Ru <64388316+sean-ru@users.noreply.github.com>
2022-07-23 11:48:44 -0500

    compiles and works

--- Greedy_Algorithms_DAC/largest_number.cpp
+++ Greedy_Algorithms_DAC/largest_number.cpp
@@ -4,48 +4,36 @@ using namespace std;
 
 class Solution {
 public:
-    string greatestNumber(vector<string>& vec) {
-        string greatest = vec[0];
-        for (int i = 1; i < vec.size(); ++i) {
-            if (greatest.length() == vec[i].length()) {
-                if (greatest.substr(0, 1) == vec[i].substr(0, 1)) {
-                    if (vec[i].substr(1, 2).compare(greatest.substr(1,2)) > 0) {
-                        greatest = vec[i];
-                    }
-                }
-                if (vec[i].substr(0, 1).compare(greatest.substr(0, 1)) > 0) {
-                    greatest = vec[i];
-                }
-            }
-            else {
-                if (vec[i].substr(0, 1).compare(greatest.substr(0, 1)) == 0) {
-                    if (vec[i].substr(1, 2).compare(greatest.substr(0, 1)) > 0) {
-                        greatest = vec[i];
-                    }
-                }
-                if (vec[i].substr(0, 1).compare(greatest.substr(0, 1)) > 0) {
-                    greatest = vec[i];
-                }
-            }
-        }
-        return greatest;
-    }
-
-    string makeString(vector<string>& vec) {
-        string ans;
-        int n = vec.size();
-        for (int i = 0; i < n; ++i) {
-            ans += greatestNumber(vec);
-            vec.erase(find(vec.begin(), vec.end(), greatestNumber(vec)));
-        }
-        return ans;        
+    static bool comp(string& s1, string& s2) {
+        if ((s1+s2) > (s2+s1)) {
+            return true;
+        }     
+        return false;
     }
     
     string largestNumber(vector<int>& nums) {
-        vector<string> numStrings;
+        vector<string> vec;
+        string ans;
+        
         for (auto& num : nums) {
-            numStrings.push_back(to_string(num));
+            vec.push_back(to_string(num));
+        }    
+        
+        sort(vec.begin(), vec.end(), comp);
+        
+        for (auto& it : vec)
+            ans += it;
+        
+        int i = 0;
+        
+        while (i < ans.length() && ans[i] == '0') {
+            i++;
         }
-        return makeString(numStrings);
+        
+        if (i == ans.length()) {
+            return "0";
+        }
+        
+        return ans;
     }
 };
