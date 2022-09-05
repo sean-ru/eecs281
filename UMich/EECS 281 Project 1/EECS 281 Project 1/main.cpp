4a3cc5ef76718e53a721d6d5d6287f4a7dc35d4d
Sean Ru <64388316+sean-ru@users.noreply.github.com>
2022-09-04 23:38:18 -0400

    more progress on p0 + started p1

---
+++ UMich/EECS 281 Project 1/EECS 281 Project 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  EECS 281 Project 1
+//
+//  Created by Sean Ru on 9/2/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
