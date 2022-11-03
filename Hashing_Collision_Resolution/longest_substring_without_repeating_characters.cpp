bbea8e1ab6192215b188c39251611ccc9c0b4630
Sean Ru <64388316+sean-ru@users.noreply.github.com>
2022-11-03 16:51:20 -0400

    did another problem

---
+++ Hashing_Collision_Resolution/longest_substring_without_repeating_characters.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int l = 0;
+        int r = 0;
+        int longest = 0;
+        unordered_set<char> us;
+        while (r < s.length()) {
+            if (us.find(s[r]) == us.end()) {
+                us.insert(s[r]);
+                longest = longest > r-l+1 ? longest : r-l+1;
+                ++r;
+            }
+            else {
+                us.erase(s[l]);
+                ++l;
+            }
+        }
+        return longest;
+    }
+};
